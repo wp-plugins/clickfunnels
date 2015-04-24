8278ce29fb4890febe2b61f9cb5ee95091054d8a
clickfunnels.com <clickfunnels.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-24 04:35:53 +0000

    New Update
    git-svn-id: https://plugins.svn.wordpress.org/clickfunnels/tags/1.0.4@1144155 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div class="copyrightInfo">
+    <p><strong style="float: right;margin-right: 20px;font-weight: 200">  -- Running version 1.0.4</strong> Copyright 2015+ &copy; Eitson, LLC</p>
+</div>
+<style>
+	.copyrightInfo {
+	    width: 780px;
+	    padding: 0 10px;
+	    float: left;
+	    color: #999;
+	    font-size: 11px;
+	    padding-top: 10px;
+	}
+	.copyrightInfo p {
+	    font-size: 11px;
+	}
+</style>
